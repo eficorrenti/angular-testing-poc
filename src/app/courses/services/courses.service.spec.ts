f5875d00ecb0c3182fa8edd24d6c8a8926683c87
secorrenti <secorrenti@gmail.com>
2020-05-12 15:15:02 +0300

    testing findAllCourses on CoursesService

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,3 +1,4 @@
+import { COURSES } from './../../../../server/db-data';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CoursesService } from './courses.service';
 import { TestBed } from '@angular/core/testing';
@@ -22,6 +23,31 @@ describe('CoursesService', () => {
 
     coursesService = TestBed.get(CoursesService);
     httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+
+  it('Should retrieve all courses', () => {
+
+    const sub = coursesService.findAllCourses().subscribe(courses => {
+
+      sub.unsubscribe();
+
+      expect(courses).toBeTruthy('No courses returned!');
+
+      expect(courses.length).toBe(12, 'Incorrect number of courses');
+
+      const couse = courses.find(row => row.id === 12);
+
+      expect(couse.titles.description).toBe('Angular Testing Course');
+
+    });
+
+    const req = httpTestingController.expectOne('/api/courses');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush({payload: Object.values(COURSES)});
+
   });
 
 
