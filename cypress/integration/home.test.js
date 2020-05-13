81eac04a687641a73e4f86e752655fde94c7e3b9
secorrenti <secorrenti@gmail.com>
2020-05-14 01:08:20 +0300

    Cypress - first setup

---
+++ cypress/integration/home.test.js
@@ -0,0 +1,28 @@
+
+
+
+
+describe('Home Page', () => {
+
+  it('Should display a list of courses', () => {
+
+    cy.fixture('courses.json').as('coursesJSON');
+
+    cy.server();
+
+    cy.route('/api/courses', '@coursesJSON').as('courses');
+
+    cy.visit('/');
+
+    cy.contains('All Courses');
+
+    cy.wait('@courses');
+
+    cy.get('mat-card').should('have.length', 9);
+
+    console.log(cy);
+
+  });
+
+});
+
